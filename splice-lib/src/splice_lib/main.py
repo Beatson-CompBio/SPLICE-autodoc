da86d4b08e966cfbfac24b297858f9b58ccd6a41
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-14 15:55:53 +0100

    Update main.py

--- splice-lib/src/splice_lib/main.py
+++ splice-lib/src/splice_lib/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from splice-pipline!")
+import uuid
 
+def main():
+    return print(random_number())
+
+def random_number() -> int:
+    """
+    This function generates a random UUID code.
+
+    :param None:
+    :return: uuid.
+    :rtype: int
+    """
+    return uuid.uuid4()
 
 if __name__ == "__main__":
     main()
